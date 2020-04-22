e60f26feb4cb52d979142ad5fe8504ca799e798a
Emanuele Pavanello <e.pavanello@teamsystem.com>
2020-04-22 22:18:09 +0200

    disabled button

--- components/myButton.tsx
+++ components/myButton.tsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 import { Text, View, SafeAreaView, ScrollView, Picker, Button, TouchableOpacity, StyleProp, ViewStyle } from "react-native";
 import { t } from "react-native-tailwindcss";
 
-type MyButtonProps = { onPress?: () => void; children: string };
-export default function MyButton({ onPress, children }: MyButtonProps) {
+type MyButtonProps = { onPress?: () => void; children: string, disabled?: boolean };
+export default function MyButton({ onPress, children, disabled }: MyButtonProps) {
   return (
-    <TouchableOpacity style={[t.bgBlue600, t.mL2, t.pY2, t.pX4, t.rounded]} onPress={onPress}>
+    <TouchableOpacity disabled={disabled} style={[disabled ? t.bgBlue300 : t.bgBlue600, t.mL2, t.pY2, t.pX4, t.rounded]} onPress={onPress}>
       <Text style={[t.textWhite, t.fontMedium]}>{children}</Text>
     </TouchableOpacity>
   );
