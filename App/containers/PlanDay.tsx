220d028097efd96c6fa2340c14f9f45114787127
Emanuele Pavanello <e.pavanello@teamsystem.com>
2020-05-14 12:33:35 +0200

    pinchToZoom

--- App/containers/PlanDay.tsx
+++ App/containers/PlanDay.tsx
@@ -1,9 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Text, View, ScrollView, TouchableOpacity } from "react-native";
 import { t } from "react-native-tailwindcss";
 import ManageTicket, { ActivityType, PlanType } from "./manageActivity";
 import moment from "moment";
 import { getTime, getHours, getMinutes } from "../shared/utilities";
+
+import {
+  PinchGestureHandler,
+  PinchGestureHandlerGestureEvent,
+  State,
+  PinchGestureHandlerStateChangeEvent,
+} from "react-native-gesture-handler";
 
 type PlanDayProps = {
   dayPlan: PlanType[];
@@ -17,6 +24,8 @@ type PlanDayProps = {
 export default function PlanDay({ dayPlan, start, end, breakStart, breakEnd, stepCents, updatePlan }: PlanDayProps) {
   const [plan, setPlan] = useState<PlanType[]>([]);
   const [planToEdit, setPlanToEdit] = useState<PlanType>();
+
+  const [rowHeight, setRowHeight] = useState(80);
 
   useEffect(() => {
     const fixedPlan = [];
@@ -48,7 +57,7 @@ export default function PlanDay({ dayPlan, start, end, breakStart, breakEnd, ste
   };
 
   const fixIndex = (index: number) => {
-    if (getHours(index + 1, stepCents, start) >= breakStart) {
+    if (getHours(index + 1, stepCents, start) > breakStart) {
       return index + Math.round(((breakEnd - breakStart) * 100) / stepCents);
     }
     return index;
@@ -62,48 +71,66 @@ export default function PlanDay({ dayPlan, start, end, breakStart, breakEnd, ste
     return result;
   };
 
+  const rowHeightBegin = useRef(0);
+
+  const onPinch = (props: PinchGestureHandlerGestureEvent) => {
+    const newHeight = Math.max(30, Math.min(80, rowHeightBegin.current * props.nativeEvent.scale));
+    setRowHeight(newHeight);
+  };
+
+  const onPinchStateChange = (props: PinchGestureHandlerStateChangeEvent) => {
+    if (props.nativeEvent.state == State.BEGAN) {
+      rowHeightBegin.current = rowHeight;
+    }
+  };
   return (
     <>
-      <ScrollView style={[t.flexShrink]}>
-        {plan.map((p, i) => (
-          <View
-            key={i}
-            style={[
-              t.borderB,
-              t.borderBlue200,
-              isBreak(p.index) ? t.borderB2 : {},
-              i == 0 ? { ...t.borderT, ...t.borderBlue200 } : {},
-              isCurrentStep(p.index) ? t.bgBlue100 : {},
-            ]}
-          >
-            <TouchableOpacity style={[t.flex, t.flexRow, t.wFull]} onPress={() => setPlanToEdit(p)}>
-              <View style={[t.w16, t.borderR, t.borderBlue200, t.p2, t.flex, t.flexRow, t.justifyEnd]}>
-                <Text>{getTime(p.index, start, stepCents, breakStart, breakEnd)}</Text>
-              </View>
-              {p.activity ? (
-                <>
-                  <View style={[t.flex1, t.pL2, t.flex, t.flexCol, t.justifyCenter, t.itemsStart]}>
-                    <Text style={[t.textXs]}>{p.activity.procedura.label}</Text>
-                    <Text style={[t.textXs]}>{p.activity.causale.label}</Text>
-                  </View>
-                  <View style={[t.flex1, t.pR2, t.flex, t.flexCol, t.justifyCenter, t.itemsStart]}>
-                    <View style={[t.flex, t.flexRow, t.itemsBaseline]}>
-                      <Text style={[t.textXs, t.fontSemibold]}>Ticket: </Text>
-                      <Text style={[t.textXs]}>{p.activity.ticket || ""}</Text>
+      <PinchGestureHandler onGestureEvent={onPinch} onHandlerStateChange={onPinchStateChange}>
+        <ScrollView style={[t.flexShrink]}>
+          {plan.map((p, i) => (
+            <View
+              key={i}
+              style={[
+                t.borderB,
+                t.borderBlue200,
+                isBreak(p.index) ? t.borderB2 : {},
+                i == 0 ? { ...t.borderT, ...t.borderBlue200 } : {},
+                isCurrentStep(p.index) ? t.bgBlue100 : {},
+              ]}
+            >
+              <TouchableOpacity style={[t.flex, t.flexRow, t.wFull, { height: rowHeight }]} onPress={() => setPlanToEdit(p)}>
+                <View style={[t.w16, t.borderR, t.borderBlue200, t.p2, t.flex, t.flexRow, t.justifyEnd]}>
+                  <Text>{getTime(p.index, start, stepCents, breakStart, breakEnd)}</Text>
+                </View>
+                {p.activity ? (
+                  <View style={[t.p2, t.flex1, t.flex, t.flexRow]}>
+                    {rowHeight > 50 && (
+                      <View style={[t.flex1, t.flex, t.flexCol, t.justifyBetween, t.itemsStart]}>
+                        <Text style={[t.textXs, t.fontSemibold]}>{p.activity.procedura.label}</Text>
+                        <Text style={[t.textXs, t.italic]}>{p.activity.causale.label}</Text>
+                      </View>
+                    )}
+                    <View style={[t.flex1, t.flex, t.flexCol, t.justifyBetween, t.itemsStart]}>
+                      {rowHeight > 75 && (
+                        <View style={[t.flex, t.flexCol, t.itemsBaseline]}>
+                          <Text style={[t.textXs, t.fontSemibold]}>Ticket: </Text>
+                          <Text style={[t.textXs]}>{p.activity.ticket || ""}</Text>
+                        </View>
+                      )}
+                      <View style={[t.flex, t.flexCol, t.itemsBaseline]}>
+                        {rowHeight > 40 && <Text style={[t.textXs, t.fontSemibold]}>Info: </Text>}
+                        <Text style={[t.textXs]}>{p.activity.info}</Text>
+                      </View>
                     </View>
-                    <View style={[t.flex, t.flexRow, t.itemsBaseline]}>
-                      <Text style={[t.textXs, t.fontSemibold]}>Info: </Text>
-                      <Text style={[t.textXs]}>{p.activity.info}</Text>
-                    </View>
                   </View>
-                </>
-              ) : (
-                <View style={[t.flex1]}></View>
-              )}
-            </TouchableOpacity>
-          </View>
-        ))}
-      </ScrollView>
+                ) : (
+                  <View style={[t.flex1]}></View>
+                )}
+              </TouchableOpacity>
+            </View>
+          ))}
+        </ScrollView>
+      </PinchGestureHandler>
       {!!planToEdit && (
         <ManageTicket
           selectedPlan={planToEdit}
